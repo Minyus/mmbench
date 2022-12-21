31021efc4141db71b655eb9d3bcea88b1170cf37
Minyus <Minyus@github.com>
2022-12-22 00:41:42 +0800

    Add MaxVit from timm

---
+++ configs/_base_/models/maxvit_nano_rw_256.py
@@ -0,0 +1,14 @@
+# model settings
+model = dict(
+    type="ImageClassifier",
+    backbone=dict(
+        type="TIMMBackbone", model_name="maxvit_nano_rw_256", pretrained=False
+    ),
+    neck=dict(type="GlobalAveragePooling"),
+    head=dict(
+        type="LinearClsHead",
+        num_classes=10,
+        in_channels=512,
+        loss=dict(type="CrossEntropyLoss", loss_weight=1.0),
+    ),
+)
